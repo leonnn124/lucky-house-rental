eae19d1539e9a714a695df7d3ddba8af477f0887
leonnn124 <leonnnx124@gmail.com>
2025-02-16 17:32:35 +0800

    2.1.0

--- src/section/location/Location.jsx
+++ src/section/location/Location.jsx
@@ -13,6 +13,8 @@ import South from '../../assets/images/south.png'
 import East from '../../assets/images/east.png'
 import West from '../../assets/images/west.png'
 import North from '../../assets/images/north.png'
+import Fengyuan from '../../assets/images/fengyuan.png'
+import Wuri from '../../assets/images/wuri.png'
 import { Container, Tab, Tabs } from 'react-bootstrap'
 import './Location.scss'
 
@@ -34,9 +36,9 @@ const Location = () => {
 
     React.useEffect(() => {
         inViews.forEach((inView, index) => {
-        if (inView) {
-            controls.start(index === 0 ? 'visible' : 'visible');
-        }
+            if (inView) {
+                controls.start(index === 0 ? 'visible' : 'visible');
+            }
         });
     }, [controls, inViews]);
 
@@ -60,26 +62,32 @@ const Location = () => {
                     variants={simpleVariants}
                     transition={{ duration: 0.5 }}
                     className='location-content'>
-                        <picture>
-                            <img src={MapImg} alt="台中市地圖" className='img-fluid'/>
-                        </picture>
-                        <div className="location-dot futan"></div>
-                        <div className="location-dot chongde"></div>
-                        <div className="location-dot houzhuang"></div>
-                        <div className="location-dot dayi"></div>
-                        <div className="location-dot changping"></div>
-                        <div className="location-dot beiping"></div>
-                        <div className="location-dot wenhua"></div>
-                        <div className="location-dot sheraton"></div>
-                        <div className="location-dot yunjiang"></div>
-                        <div className="location-dot zhennan"></div>
-                        <div className="location-dot haohan"></div>
-                        <div className="location-dot shengmeijimei"></div>
-                        <div className="location-dot tanfu"></div>
-                        <div className="location-dot dongshan"></div>
-                        <div className="location-dot qingdao"></div>
-                        <div className="location-dot huayuanxindu"></div>
-                        <div className="location-dot taiwandadao"></div>
+                    <picture>
+                        <img src={MapImg} alt="台中市地圖" className='img-fluid' />
+                    </picture>
+                    <div className="location-dot futan"></div>
+                    <div className="location-dot chongde"></div>
+                    <div className="location-dot houzhuang"></div>
+                    <div className="location-dot dayi"></div>
+                    <div className="location-dot changping"></div>
+                    <div className="location-dot beiping"></div>
+                    <div className="location-dot wenhua"></div>
+                    <div className="location-dot sheraton"></div>
+                    <div className="location-dot yunjiang"></div>
+                    <div className="location-dot zhennan"></div>
+                    <div className="location-dot haohan"></div>
+                    <div className="location-dot shengmeijimei"></div>
+                    <div className="location-dot tanfu"></div>
+                    <div className="location-dot dongshan"></div>
+                    <div className="location-dot qingdao"></div>
+                    <div className="location-dot huayuanxindu"></div>
+                    <div className="location-dot taiwandadao"></div>
+                    <div className="location-dot royal"></div>
+                    <div className="location-dot shenglin"></div>
+                    <div className="location-dot xinglin"></div>
+                    <div className="location-dot yaxin"></div>
+                    <div className="location-dot chengong"></div>
+                    <div className="location-dot yinghua"></div>
                 </motion.div>
 
                 <motion.div
@@ -89,66 +97,78 @@ const Location = () => {
                     variants={simpleVariants}
                     transition={{ duration: 0.5 }}
                     className='location-tab'>
-                        <Tabs
-                            id="location-tab-nav"
-                            activeKey={key}
-                            onSelect={(k) => setKey(k)}
-                            className="mb-3 border-0"
-                        >
-                            <Tab eventKey="beitun" title="北屯區">
-                                <img src={Beitun} alt="北屯區" className='img-fluid'/>
-                                <div className="location-dot chongde lg"></div>
-                                <div className="location-dot houzhuang lg"></div>
-                                <div className="location-dot dayi lg"></div>
-                                <div className="location-dot changping lg"></div>
-                                <div className="location-dot beiping lg"></div>
-                                <div className="location-dot dongshan lg"></div>
-                                <div className="location-dot huayuanxindu lg"></div>
-                            </Tab>
-                            <Tab eventKey="north" title="北區" className='text-center' style={{width: '250px'}}>
-                                <img src={North} alt="北區" className='img-fluid' width={400}/>
-                                <div className="location-dot qingdao lg"></div>
-                            </Tab>
-                            <Tab eventKey="west" title="西區" className='text-center' style={{width: '250px'}}>
-                                <img src={West} alt="西區" className='img-fluid' width={400}/>
-                            </Tab>
-                            <Tab eventKey="east" title="東區" className='text-center' style={{width: '250px'}}>
-                                <img src={East} alt="東區" className='img-fluid' width={400}/>
-                                <div className="location-dot haohan lg"></div>
-                            </Tab>
-                            <Tab eventKey="south" title="南區" className='text-center' style={{width: '250px'}}>
-                                <img src={South} alt="南區" className='img-fluid' width={400}/>
-                                <div className="location-dot yunjiang lg"></div>
-                            </Tab>
-                            <Tab eventKey="xitun" title="西屯區">
-                                <img src={Xitun} alt="西屯區" className='img-fluid'/>
-                                <div className="location-dot wenhua lg"></div>
-                                <div className="location-dot sheraton lg"></div>
-                            </Tab>
-                            <Tab eventKey="shalu" title="沙鹿區">
-                                <img src={Shalu} alt="沙鹿區" className='img-fluid'/>
-                                <div className="location-dot zhennan lg"></div>
-                                <div className="location-dot taiwandadao lg"></div>
-                            </Tab>
-                            <Tab eventKey="tanzi" title="潭子區">
-                                <img src={Tanzi} alt="潭子區" className='img-fluid'/>
-                                <div className="location-dot futan lg"></div>
-                                <div className="location-dot tanfu lg"></div>
-                            </Tab>
-                            <Tab eventKey="nantun" title="南屯區">
-                                <img src={Nantun} alt="南屯區" className='img-fluid'/>
-                                <div className="location-dot shengmeijimei lg"></div>
-                            </Tab>
-                            <Tab eventKey="taiping" title="太平區">
-                                <img src={Taiping} alt="太平區" className='img-fluid'/>
-                            </Tab>
-                            <Tab eventKey="dali" title="大里區">
-                                <img src={Dali} alt="大里區" className='img-fluid'/>
-                            </Tab>
-                            <Tab eventKey="daya" title="大雅區">
-                                <img src={Daya} alt="大雅區" className='img-fluid'/>
-                            </Tab>
-                        </Tabs>
+                    <Tabs
+                        id="location-tab-nav"
+                        activeKey={key}
+                        onSelect={(k) => setKey(k)}
+                        className="mb-3 border-0"
+                    >
+                        <Tab eventKey="beitun" title="北屯區">
+                            <img src={Beitun} alt="北屯區" className='img-fluid' />
+                            <div className="location-dot chongde lg"></div>
+                            <div className="location-dot houzhuang lg"></div>
+                            <div className="location-dot dayi lg"></div>
+                            <div className="location-dot changping lg"></div>
+                            <div className="location-dot beiping lg"></div>
+                            <div className="location-dot dongshan lg"></div>
+                            <div className="location-dot huayuanxindu lg"></div>
+                            <div className="location-dot yaxin lg"></div>
+                        </Tab>
+                        <Tab eventKey="north" title="北區" className='text-center' style={{ width: '250px' }}>
+                            <img src={North} alt="北區" className='img-fluid' width={400} />
+                            <div className="location-dot qingdao lg"></div>
+                        </Tab>
+                        <Tab eventKey="west" title="西區" className='text-center' style={{ width: '250px' }}>
+                            <img src={West} alt="西區" className='img-fluid' width={400} />
+                        </Tab>
+                        <Tab eventKey="east" title="東區" className='text-center' style={{ width: '250px' }}>
+                            <img src={East} alt="東區" className='img-fluid' width={400} />
+                            <div className="location-dot haohan lg"></div>
+                        </Tab>
+                        <Tab eventKey="south" title="南區" className='text-center' style={{ width: '250px' }}>
+                            <img src={South} alt="南區" className='img-fluid' width={400} />
+                            <div className="location-dot yunjiang lg"></div>
+                        </Tab>
+                        <Tab eventKey="xitun" title="西屯區">
+                            <img src={Xitun} alt="西屯區" className='img-fluid' />
+                            <div className="location-dot wenhua lg"></div>
+                            <div className="location-dot sheraton lg"></div>
+                            <div className="location-dot xinglin lg"></div>
+                        </Tab>
+                        <Tab eventKey="shalu" title="沙鹿區">
+                            <img src={Shalu} alt="沙鹿區" className='img-fluid' />
+                            <div className="location-dot zhennan lg"></div>
+                            <div className="location-dot taiwandadao lg"></div>
+                        </Tab>
+                        <Tab eventKey="tanzi" title="潭子區">
+                            <img src={Tanzi} alt="潭子區" className='img-fluid' />
+                            <div className="location-dot futan lg"></div>
+                            <div className="location-dot tanfu lg"></div>
+                            <div className="location-dot royal lg"></div>
+                        </Tab>
+                        <Tab eventKey="nantun" title="南屯區">
+                            <img src={Nantun} alt="南屯區" className='img-fluid' />
+                            <div className="location-dot shengmeijimei lg"></div>
+                        </Tab>
+                        <Tab eventKey="taiping" title="太平區">
+                            <img src={Taiping} alt="太平區" className='img-fluid' />
+                            <div className="location-dot shenglin lg"></div>
+                        </Tab>
+                        <Tab eventKey="dali" title="大里區">
+                            <img src={Dali} alt="大里區" className='img-fluid' />
+                        </Tab>
+                        <Tab eventKey="daya" title="大雅區">
+                            <img src={Daya} alt="大雅區" className='img-fluid' />
+                        </Tab>
+                        <Tab eventKey="fengyuan" title="豐原區">
+                            <img src={Fengyuan} alt="豐原區" className='img-fluid' />
+                            <div className="location-dot chengong lg"></div>
+                        </Tab>
+                        <Tab eventKey="wuri" title="烏日區">
+                            <img src={Wuri} alt="烏日區" className='img-fluid' />
+                            <div className="location-dot yinghua lg"></div>
+                        </Tab>
+                    </Tabs>
                 </motion.div>
             </Container>
         </section>
